82249c2a199ed353c12166460ca8681817a2f92a
Nicolo Ruffini <nicolo@Nicolos-MacBook-Air.local>
2020-12-06 18:56:50 -0500

    working on getting 25 at a time

--- turnover_margin.py
+++ turnover_margin.py
@@ -1,35 +1,51 @@
 from bs4 import BeautifulSoup
 import requests
 import os
+import sqlite3
+
+def setUpDatabase(db_name):
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+db_name)
+    cur = conn.cursor()
+    return cur, conn
 
 def get_turnover_margin():
 
     urls = ['https://www.ncaa.com/stats/football/fbs/current/team/29', 'https://www.ncaa.com/stats/football/fbs/current/team/29/p2', 'https://www.ncaa.com/stats/football/fbs/current/team/29/p3']
     
+    cur.execute('CREATE TABLE IF NOT EXISTS team_turnovers (school_id INTEGER PRIMARY KEY, turnover_margin)')
+    cur.execute('SELECT school_id from team_turnovers')
+
+    # Checking teams in db and creating list
+    data = cur.fetchall()
+    keep_track_ids = []
+    for d in data:
+        keep_track_ids.append(d[0])
+
+    
     teams = []
     turnover_margin = []
     for url in urls:
-        u = url
-        resp = requests.get(u)
-        if resp.ok:
-            soup = BeautifulSoup(resp.content, 'lxml')
-            schools = soup.find_all('a', class_ = 'school')
-            for tag in schools:
-                school = tag.text
-                teams.append(school)
+        resp = requests.get(url)
+        soup = BeautifulSoup(resp.content, 'lxml')
+        schools = soup.find_all('a', class_ = 'school')
 
-            body = soup.find('tbody')
-            tags = body.find_all('tr')
-            for tag in tags:
-                stats = tag.find_all('td')
-                lst = []
+        for tag in schools:
+            school = tag.text
 
-                for stat in stats:
-                    lst.append(stat.text)
+            teams.append(school)
 
-                turnover_margin.append(lst[9])
-        else:
-            print('---response error---')
+        body = soup.find('tbody')
+        tags = body.find_all('tr')
+        for tag in tags:
+            stats = tag.find_all('td')
+            lst = []
+
+            for stat in stats:
+                lst.append(stat.text)
+
+            turnover_margin.append(lst[9])
+
 
     team_turnover = {}
     for i in range(len(teams)):
@@ -38,5 +54,8 @@ def get_turnover_margin():
         team_turnover[team] = margin
 
     return team_turnover
+
+
+cur, conn = setUpDatabase('ncaa_football_stats.db')
 
 get_turnover_margin()

d305d147222758db2e27a27227d7db6756b7dfdc
Nicolo Ruffini <nicolo@Nicolos-MacBook-Air.local>
2020-12-07 14:21:07 -0500

    25 at a time

--- turnover_margin.py
+++ turnover_margin.py
@@ -9,14 +9,14 @@ def setUpDatabase(db_name):
     cur = conn.cursor()
     return cur, conn
 
-def get_turnover_margin():
+def get_turnover_margin(cur, conn):
 
     urls = ['https://www.ncaa.com/stats/football/fbs/current/team/29', 'https://www.ncaa.com/stats/football/fbs/current/team/29/p2', 'https://www.ncaa.com/stats/football/fbs/current/team/29/p3']
     
     cur.execute('CREATE TABLE IF NOT EXISTS team_turnovers (school_id INTEGER PRIMARY KEY, turnover_margin)')
     cur.execute('SELECT school_id from team_turnovers')
 
-    # Checking teams in db and creating list
+    # Checking team ids in db and creating list
     data = cur.fetchall()
     keep_track_ids = []
     for d in data:
@@ -28,15 +28,19 @@ def get_turnover_margin():
     for url in urls:
         resp = requests.get(url)
         soup = BeautifulSoup(resp.content, 'lxml')
+
+        # finding tags with schools
         schools = soup.find_all('a', class_ = 'school')
+
+        # finding stats for schools
+        body = soup.find('tbody')
+        tags = body.find_all('tr')
 
         for tag in schools:
             school = tag.text
 
             teams.append(school)
 
-        body = soup.find('tbody')
-        tags = body.find_all('tr')
         for tag in tags:
             stats = tag.find_all('td')
             lst = []
@@ -55,7 +59,26 @@ def get_turnover_margin():
 
     return team_turnover
 
+def add_turnovers_to_db(cur, conn, dic):
+    
+    
+    i = 0
+    for team in dic:
+        if i == 25:
+            break
+        cur.execute('SELECT id from teams WHERE team = ?', (team, ))
+        team_id = cur.fetchone()[0]
+        try:
+            cur.execute('INSERT INTO team_turnovers (school_id, turnover_margin) VALUES (?, ?)', (team_id, dic[team]))
+            i += 1
+        except:
+            print('Team already in database')
+    conn.commit()
+    return None
+
+
+
 
 cur, conn = setUpDatabase('ncaa_football_stats.db')
-
-get_turnover_margin()
+turnover_dic = get_turnover_margin(cur, conn)
+add_turnovers_to_db(cur, conn, turnover_dic)
